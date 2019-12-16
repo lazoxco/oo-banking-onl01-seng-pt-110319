21a68dcea64691911b2189563dc343f6fcaab267
Nory Lazo <nlazo@icloud.com>
2019-12-16 00:17:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,10 +5,10 @@ class Transfer
   # attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(sender, receiver, amount)
-    # @sender = sender
-    # @receiver = receiver
-    # @status = "pending"
-    # @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
   end 
   
   def valid? 
