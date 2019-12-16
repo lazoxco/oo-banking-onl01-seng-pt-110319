07220a336827fdc6f2d985586eb50616d53c5f17
Nory Lazo <nlazo@icloud.com>
2019-12-16 00:15:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,13 +2,13 @@ require "pry"
 
 class Transfer
   # your code here
-  attr_accessor :sender, :receiver, :amount, :status
+  # attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @status = "pending"
-    @amount = amount
+    # @sender = sender
+    # @receiver = receiver
+    # @status = "pending"
+    # @amount = amount
   end 
   
   def valid? 
